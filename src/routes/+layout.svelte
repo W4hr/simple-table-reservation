cb47e19eec26d2a524d600dbc00a02398e5e59e3
W4hr <jakob.butze@gmail.com>
2025-05-05 13:32:38 +0200

    Added Footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,3 +5,14 @@
 </script>
 
 {@render children()}
+
+<footer class="bg-indigo-600 text-center py-4 px-4 text-sm text-white bg-gray-100 border-t border-gray-300">
+	<p>&copy; 2025 Jakob Martin Butze. All rights reserved.</p>
+	<p>
+	  	Released under the <a href="https://opensource.org/licenses/MIT" target="_blank" rel="noopener noreferrer">MIT License</a>.
+	</p>
+	<p>
+	  	View source on <a href="https://github.com/W4hr/simple-table-reservation" target="_blank" rel="noopener noreferrer">GitHub</a>.
+	</p>
+</footer>
+  
